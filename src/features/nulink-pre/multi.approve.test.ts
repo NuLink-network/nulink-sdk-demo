3c290ba7437ad9bf26e54e20dd9cbea471a977b7
andi <iandy2233@gmail.com>
2024-06-05 15:58:55 +0800

    update sdk version

--- src/features/nulink-pre/multi.approve.test.ts
+++ src/features/nulink-pre/multi.approve.test.ts
@@ -268,7 +268,7 @@ export const run = async () => {
 
     //Bob requests permission to use the file for 7 days
     try {
-      await pre.applyForDataUsagePermission([applyFileId], accountBob, 7);
+      await pre.applyForDataUsagePermission(applyFileId, accountBob, 7);
     } catch (e) {
       console.log(`bob_${index} apply file failed`, e);
       assert(false);
